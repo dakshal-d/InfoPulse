f6ddb35df8fdc32202235d3d92d45331666f678a
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-09-28 17:03:29 +0530

    minor update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,7 @@ const NavBar = (props) => {
     <>
       <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/"><strong>NewZ BuzZ</strong></Link>
+          <Link className="navbar-brand" to="/"><strong>Info Pulse</strong></Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
